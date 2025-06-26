b6020ced7e0a9a06c9553c398ae3013307308870
ronaldoLPFilho <ronaldo.luizcp@gmail.com>
2025-06-26 20:20:29 -0300

    feat: :sparkles: Categories list - not done yet

---
+++ src/features/categories/components/CategoryList.tsx
@@ -0,0 +1,34 @@
+import { li } from "framer-motion/client";
+import { Category } from "../types/Category";
+
+interface Props {
+    categories: Category[];
+    onCategoriesUpdated: () => void; 
+}
+
+export function CategoryList({categories, onCategoriesUpdated}: Props ){
+    const handleDelete = async (id: string) => {
+        if(confirm("Deseja excluir essa categoria?")){
+            //await deleteCategory(id);
+            onCategoriesUpdated();
+        }
+    };
+
+    return (
+        <div className="flex flex-col gap-3 border border-gray-200 rounded-lg p-4">
+            <h2 className="text-lg font-semibold">Categorias Cadastradas</h2>
+            <ul className="space-y-2">
+                {categories.map((category) => (
+                    <li
+                        key={category.id}
+                        className="flex items-center justify-between p3- rounded border"
+                        style={{backgroundColor: category.color}}
+                    >
+                        <span className="font-medium text-white drop-shadow">{category.name}</span>
+                        <button onClick={() => handleDelete(category.id)}>🗑️</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
--- src/features/categories/pages/CategoryPage.tsx
+++ src/features/categories/pages/CategoryPage.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 import {Category} from "../types/Category"
-import { useNavigate } from "react-router-dom";
-import { div } from "framer-motion/client";
 import { CategoryForm } from "../components/CategoryForm";
+import { CategoryList } from "../components/CategoryList";
+import { getAllCategories } from "../services/CategoryService";
 
 export function CategoryPage(){
     const [categories, setCategories] = useState<Category[]>([]);
 
     const loadCategories = async () => {
         try{
-            //const data= await getAllCategories();
-            //setCategories(data);
+            const data= await getAllCategories();
+            setCategories(data);
         }catch(err){
             console.error("Error ao carregar categorias")
         }
@@ -23,6 +23,8 @@ export function CategoryPage(){
     return (
         <div className="max-w-2xl mx-auto p-4">
             <CategoryForm onCategoryCreated={loadCategories} />
+            <div className="mt-5"></div>
+            <CategoryList categories={categories} onCategoriesUpdated={loadCategories}/>
         </div>
     )
 }
