c1436346bd6441f1ed6c86986de496b7d3483732
ronaldoLPFilho <ronaldo.luizcp@gmail.com>
2025-07-12 12:33:53 -0300

    feat: :sparkles: adding calendar on header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,14 +1,17 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../features/auth/contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
-import { CalendarDays, Search, User } from "lucide-react";
+import { Search, User } from "lucide-react";
+import { DatePicker } from "../components/DatePicker";
 
 export function Header() {
   const { username } = useContext(AuthContext);
   const navigate = useNavigate();
 
+  const [selectedDate, setSelectedDate] = useState<Date| undefined>();
+
   return (
-    <header className="bg-white py-6 shadow-sm flex justify-center">
+    <header className="bg-white py-6 shadow-sm flex justify-center relative z-10">
       <div className="w-[70%] max-w-6xl">
         <div className="flex items-center gap-4 mb-4">
           <div className="bg-purple-500 text-white rounded-full w-10 h-10 flex items-center justify-center">
@@ -20,7 +23,7 @@ export function Header() {
           </div>
         </div>
 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 relative">
           <div className="flex items-center border border-gray-300 rounded-md flex-1 px-3 py-2">
             <Search className="text-gray-500 w-4 h-4 mr-2" />
             <input
@@ -30,10 +33,7 @@ export function Header() {
             />
           </div>
 
-          <button className="flex items-center border border-gray-300 px-4 py-2 rounded-md text-sm">
-            <CalendarDays className="w-4 h-4 mr-1" />
-            Data
-          </button>
+          <DatePicker selectedDate={selectedDate} onDateSelect={setSelectedDate}/>
 
           <button className="bg-purple-500 hover:bg-purple-600 text-white px-5 py-2 rounded-md text-sm transition">
             <Search className="w-4 h-4 inline mr-1" />
